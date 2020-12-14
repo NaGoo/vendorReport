12fc1e3fb4b2a91a0fbda97888f37b61430dabf3
NaGoo <nagucsengg@gmail.com>
2020-12-14 16:29:41 +0530

    updated

--- index.js
+++ index.js
@@ -1,132 +1,65 @@
-//const xlsx = require('xlsx');
-const admin = require("firebase-admin");
-const moment = require("moment");
-const serviceAccount = require("./ra-dev-mechapp-9226f801c92c.json");
-//const ExcelJS = require('exceljs/dist/es5');
-const Excel = require("exceljs");
-
+const admin = require('firebase-admin');
+const moment = require('moment');
+const serviceAccount = require('');
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount)
 });
-let a = [
-  "20201201",
-  "20201202",
-  "20201203",
-  "20201204",
-  "20201205",
-  "20201206",
-  "20201207",
-  "20201208",
-  "20201212",
-  "20201210",
-  "20201211",
-  "20201212",
-  "20201213",
-  "20201214",
-  "20201215",
-  "20201216",
-  "20201217",
-  "20201218",
-  "20201219",
-  "20201220",
-  "20201221",
-  "20201222",
-  "20201223",
-  "20201224",
-  "20201225",
-  "20201226",
-  "20201227",
-  "20201228",
-  "20201229",
-  "20201230",
+let a = ['20201201', '20201202', '20201203', '20201204', '20201205', '20201206', '20201207', '20201208', '20201212',
+  '20201210', '20201211', '20201212', '20201213', '20201214', '20201215', '20201216', '20201217', '20201218', '20201219',
+  '20201220', '20201221', '20201222', '20201223', '20201224', '20201225', '20201226', '20201227', '20201228', '20201229', '20201230'
 ];
 
+// let a = [ '20201101', '20201102', '20201103', '20201104', '20201105', '20201106', '20201107', '20201108', '20201111',
+//   '20201110', '20201111', '20201112', '20201113', '20201114', '20201115', '20201116', '20201117', '20201118', '20201119',
+//   '20201120', '20201121', '20201122', '20201123', '20201124', '20201125', '20201126', '20201127', '20201128', '20201129', '20201130'
+// ];
 const db = admin.firestore();
-const vendorRef = db.collection("vendor");
-let workbook = new Excel.Workbook();
-let worksheet = workbook.addWorksheet("Record1");
+const vendorRef = db.collection('vendor');
 vendorRef.get().then((snapshot) => {
-  //console.log(snapshot);
-  snapshot.forEach((doc) => {
-    let mydata = doc.data(); //console.log(mydata);
-    db.collection("vendor_history")
-      .doc(doc.id)
-      .listCollections()
-      .then((myList) => {
-        let listOfCollections = [];
-        // console.log(myList);
-        myList.forEach((collection) => {
-          if (a.includes(String(collection.id))) {
-            db.collection(`vendor_history/${doc.id}/${collection.id}/`)
-              .orderBy("ts")
-              .get()
-              .then((vh) => {
-                worksheet.columns = [
-                  { header: "Time", key: "ts" },
-                  { header: "Status", key: "vs" },
-                ];
-                getDataToExport(vh).then((response) => {
-                  workbook.xlsx.writeFile("NewRecord3.xlsx");
-                });
-              });
+  snapshot.forEach(async doc => {
+    let mydata = doc.data();
+
+    let myList = await vendorHistory(doc);
+    if (myList.length == 0) {                           //console.log("My List Not Found")
+    } else {                                           // console.log("My List---",(myList))
+      for (var i = 0; i < myList.length; i++) {       //console.log("Doc-Id:---"+doc.id+" , Collection Id:---"+myList[i].id)
+        if (a.includes(String(myList[i].id))) {
+          let vh = await subVendorHistory(doc, myList[i]);  // console.log("vh length----",vh)
+          if (!vh || vh == undefined) {             //console.log("vh not Found")
+          } else {
+            let allData = [];
+            vh.forEach(vhdoc => {
+              let vhdata = vhdoc.data();
+              allData.push(vhdata)
+            })
+            let notOnDuty = allData.filter(nt_on_duty => {
+              if (nt_on_duty.vs.toUpperCase() !== 'NOT ON DUTY') {
+                return nt_on_duty
+              }
+            })                                      // console.log("vh on-duty----",notOnDuty)
+            if (notOnDuty.length > 0) {
+              let fromDate = moment.utc(notOnDuty[0].ts.toDate()).local().format().toString().replace('T', ' ').replace('+05:30', '')
+              let toDate = moment.utc(notOnDuty[notOnDuty.length - 1].ts.toDate()).local().format().toString().replace('T', ' ').replace('+05:30', '')
+              let difference1 = moment(toDate).diff(moment(fromDate), 'hours', true)
+              console.log('Vendor_id :',doc.id,'  ', 'Start :', fromDate,'   ' ,'End :', toDate,'     ' ,'Working_hours :', difference1)
+            }
           }
-        });
-      });
+        }
+      }
+    }
   });
 });
-
-function getDataToExport(vh) {
-  let tempOb = [];
+function vendorHistory(doc) {
   return new Promise((resolve) => {
-    vh.forEach((vhdoc, index) => {
-      let vhdata = vhdoc.data();
-      console.log(vhdata);
-      //console.log(vhdata.length)
-
-      if (vhdata.vs.toUpperCase() !== "NOT ON DUTY") {
-        let obj = {
-          ts: moment
-            .utc(vhdata.ts.toDate())
-            .local()
-            .format()
-            .toString()
-            .replace("T", "")
-            .replace("+05:30", ""),
-          vs: vhdata.vs,
-        };
-        tempOb.push(obj);
-      }
-      setTimeout(() => {
-        tempOb.forEach((element, index) => {
-          worksheet.addRow({ element });
-          if (index == tempOb.length - 1) {
-            workbook.xlsx.writeFile("NewRecord.xlsx");
-            resolve();
-          }
-        });
-      }, 15000);
-    });
-  });
+    db.collection('vendor_history').doc(doc.id).listCollections().then((myList) => {
+      resolve(myList)
+    }).catch([])
+  })
 }
-
-// db.collection(`vendor_history/19/20200914/`).orderBy('ts').get().then((vh) => {
-//   vh.forEach(vhdoc => {
-//     let vhdata = vhdoc.data();
-//     // if (vhdata.vs.toUpperCase() !== 'NOT ON DUTY') {
-//     // console.log(vhdata.ts.toDate());
-//     console.log(vhdoc.id, ',', moment.utc(vhdata.ts.toDate()).local().format().toString().replace('T', ' ').replace('+5:30', ''), ',', vhdata.vs);
-//     // }
-//   });
-// });
-
-// db.collection(`vendor`).get().then((vd) => {
-//   vd.forEach((vd) => {
-//     let v = vd.data
-//     console.log(v.id, v.vs, v.ts, v.VendorStatus, v.mrodt);
-//   })
-// });
-
-// var newWB = xlsx.utils.book_new();
-//                 var newWS = xlsx.utils.json_to_sheet(doc.id);
-//                 xlsx.utils.book_append_sheet (newWB, newWS, "New Data8");
-//               xlsx.writeFile(newWB, "New Data8 File.xlsx");
+function subVendorHistory(doc, collection) {
+  return new Promise((resolve) => {
+    db.collection(`vendor_history/${doc.id}/${collection.id}/`).orderBy('ts').get().then((vh) => {
+      resolve(vh)
+    }).catch([])
+  })
+}
